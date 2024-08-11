6481e6227ab660724ffa2ae6685d217448c4ffc4
idilTugba <idilgolcuk@gmail.com>
2024-08-12 00:14:10 +0300

    part1 js exercise 1.3 - 1.5

--- Part1/Step1/part1/src/components/Total.tsx
+++ Part1/Step1/part1/src/components/Total.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 const Total = (props) => {
+  const posts = props.parts;
+  const total = posts.reduce((acc, item) => item.exercises + acc, 0);
   return (
     <>
-      <p>Number of exercises {props.total}</p>
+      <p>Number of exercises {total} </p>
     </>
   );
 };
